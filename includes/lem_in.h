171f1f1f9d829dd86dbe1e60f0367f3ac448fa77
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-28 14:08:39 -0700

    Added basic structs

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 14:00:21 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/03/28 14:00:27 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/03/28 14:07:56 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,21 @@
 /*
 ** Structures
 */
+
+struct					s_ant
+{
+	//a path
+	int					number;
+	char				*location;
+};
+typedef struct s_ant	t_ant;
+
+struct					s_vertex
+{
+	char				*name;
+	t_point				pos;
+};
+typedef struct s_vertex	t_vertex;
 
 /*
 ** Functions
