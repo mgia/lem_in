0f9ea1b4e83f3799b49be533e91ef13a9208e383
mtan <findmarcg@gmail.com>
2018-03-28 17:01:30 -0700

    parsing ants and rooms without links

--- src/libft_add.c
+++ src/libft_add.c
@@ -2,11 +2,29 @@
 
 int		ft_isdigit_str(char *str)
 {
-	while (str)
+	while (*str)
 	{
 		if (!ft_isdigit(*str))
 			return (0);
 		str++;
 	}
 	return (1);
+}
+
+char	*ft_strndup(const char *s, size_t n)
+{
+	char			*res;
+	unsigned int	i;
+
+	i = 0;
+	res = malloc(sizeof(char) * (n + 1));
+	if (res == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		res[i] = s[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
--- src/main.c
+++ src/main.c
@@ -5,63 +5,108 @@
 ** 2: NO_START, 3: NO_ROOM, 4: ILLEGAL_NAME, 5: NO_PATH, 6: INVALID_ROOM
 */
 
-void	error(int i)
+void	error(const char *str)
 {
-	if (i == 0)
-		ft_putendl("Error : No or empty file");
-	else if (i == 1)
-		ft_putendl("Error : invalid ants format");
-	else if (i == 2)
-		ft_putendl("Error : no start or end");
-	else if (i == 3)
-		ft_putendl("Error : no rooms");
-	else if (i == 4)
-		ft_putendl("Error : Illegal name");
-	else if (i == 5)
-		ft_putendl("Error : no path");
-	else if (i == 6)
-		ft_putendl("Error : invalid room");
+	// if (i == 0)
+	// 	ft_putendl("Error : No or empty file");
+	// else if (i == 1)
+	// 	ft_putendl("Error : invalid ants format");
+	// else if (i == 2)
+	// 	ft_putendl("Error : no start or end");
+	// else if (i == 3)
+	// 	ft_putendl("Error : no rooms");
+	// else if (i == 4)
+	// 	ft_putendl("Error : Illegal name");
+	// else if (i == 5)
+	// 	ft_putendl("Error : no path");
+	// else if (i == 6)
+	// 	ft_putendl("Error : invalid room");
+	ft_putendl(str);
+	exit(1);
 }
 
-int		parse_ants(t_ant *info)
+void	parse_ants(t_ant *ants)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
 	if (!get_next_line(0, &line))
-	{
-		error(0);
-		return (0);
-	}
-	info->number = ft_atoi(line);
-	if (!ft_isdigit_str(line) || info->number <= 0)
-	{
-		error(1);
-		return (0);
-	}
-	return (1);
+		error("Error: No File");
+	ants->number = ft_atoi(line);
+	if (!ft_isdigit_str(line) || ants->number <= 0)
+		error("Error: No Ants");
+	ft_putendl(line);
+	free(line);
 }
 
-int		parse_rooms(t_vertex *v)
+// array of structs
+
+void	parse_rooms(t_vertex *v)
 {
-	v = NULL;
-	return (1);
+	char	*line;
+	int		start;
+	int 	end;
+	int		links;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	start = 0;
+	end = 0;
+	links = 0;
+	while (get_next_line(0, &line))
+	{
+		if (ft_strchr(line, '-'))
+		{
+			links = 1;
+			break ;
+		}
+		else if (line[0] == '#' && line[1] == '#' && ft_strstr(line, "start"))
+		{
+			start = 1;
+			v[i].type = 1;
+			i--;
+		}
+		else if (line[0] == '#' && line[1] == '#' && ft_strstr(line, "end"))
+		{
+			end = 1;
+			v[i].type = 2;
+			i--;
+		}
+		else
+		{
+			v[i].type = 0;
+			if (line[0] == 'L')
+				error("Error: Illegal name");
+			while (line[j] != ' ')
+				j++;
+			v[i].name = ft_strndup(line, j);
+		}
+		i++;
+		ft_putendl(line);
+		free(line);
+	}
+	if (!i)
+		error("Error: No rooms");
+	if (!start || !end)
+		error("Error: No Start or End");
+	if (!links)
+		error("Error: No Links");
 }
 
-int		parse_input(t_ant *ants, t_vertex *v)
+void	parse_input(t_ant *ants, t_vertex *v)
 {
-	if (!parse_ants(ants) || !parse_rooms(v))
-		return (0);
-	return (1);
+	parse_ants(ants);
+	parse_rooms(v);
 }
 
 int		main(void)
 {
 	t_ant		ants;
-	t_vertex	v;
+	t_vertex	v[42];
 
-	if (!parse_input(&ants, &v))
-		return (-1);
+	parse_input(&ants, v);
 	return (0);
 }
