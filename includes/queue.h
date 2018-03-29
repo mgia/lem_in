26ad425ff51ba8b312e788f1338086e5f94f0d41
Ivan Kozlov <ikozlov9000@gmail.com>
2018-03-28 21:00:35 -0700

    Added queue

---
+++ includes/queue.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/28 16:49:14 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/03/28 20:56:39 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef QUEUE_H
+# define QUEUE_H
+
+# include <stddef.h>
+
+/*
+** Dynamic queue based on generic list
+*/
+
+void			*q_pop(void);
+void			*q_peek(void);
+void			q_init(void);
+void			q_destroy(void);
+size_t			q_push(void *data, size_t size);
+
+#endif
---
+++ src/queue.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/28 14:14:06 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/03/28 20:58:48 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "list.h"
+
+static t_list	*g_queue;
+static size_t	g_count;
+
+/*
+** Operation:	Initializes queue.
+*/
+
+void			q_init(void)
+{
+	g_queue = NULL;
+	g_count = 0;
+}
+
+/*
+** Operation:	Frees and destroys queue.
+*/
+
+void			q_destroy(void)
+{
+	g_count = 0;
+	ft_lstdel(&g_queue, ft_free_content);
+	return ;
+}
+
+/*
+** Argument:	1. Void pointer respresenting data
+**				2. Content size
+** Operation:	Creates new node and adds it at the end of queue.
+** Return:		Size of queue.
+*/
+
+size_t			q_push(void *data, size_t size)
+{
+	t_list	*item;
+
+	item = ft_lstnew(data, size);
+	if (g_queue == NULL)
+		g_queue = item;
+	else
+		ft_lstadd(&g_queue, item);
+	return (++g_count);
+}
+
+/*
+** Operation:	Retrieves, but does not remove, the last node of queue
+** Return:		Void pointer on node's data
+*/
+
+void			*q_peek(void)
+{
+	void	*data;
+
+	data = NULL;
+	if (g_queue != NULL && g_count >= 1)
+		data = g_queue->content;
+	return (data);
+}
+
+/*
+** Operation:	Retrieves and removes the last node of queue
+** Return:		Void pointer on node's data
+*/
+
+void			*q_pop(void)
+{
+	void	*data;
+
+	data = NULL;
+	if (g_queue != NULL && g_count >= 1)
+		data = g_queue->content;
+	g_queue = ft_lstdelfirst(&g_queue, ft_save_content);
+	g_count--;
+	return (data);
+}
