a78e68a4d7c9b6e8553b1fa5677a37f53af99cba
mtan <findmarcg@gmail.com>
2018-03-31 14:21:13 -0700

    Move free functions to helpers, and added print functions

--- src/helpers.c
+++ src/helpers.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   helpers.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/31 14:12:51 by mtan              #+#    #+#             */
+/*   Updated: 2018/03/31 14:12:52 by mtan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lem_in.h"
 #include "libft.h"
 
@@ -5,4 +17,42 @@ void	error(const char *str)
 {
 	ft_putendl(str);
 	exit(1);
+}
+
+void	free_graph(t_graph *g)
+{
+	int		i;
+
+	i = -1;
+	while (++i < g->V)
+	{
+		free(g->nodes[i].name);
+		if (g->nodes[i].children)
+			ft_lstdel(&g->nodes[i].children, ft_save_content);
+	}
+	ft_memdel((void **)&g->nodes);
+}
+
+void	free_all(t_list *p, t_graph *g)
+{
+	free_graph(g);
+	ft_lstdel(&p, ft_free_content);
+}
+
+void	print_paths(t_list *p)
+{
+	while (p)
+	{
+		print_int_arr(p->content, p->content_size / 4);
+		p = p->next;
+	}
+}
+
+void	print_names(t_vertex *v, t_graph g)
+{
+	int i;
+
+	i = -1;
+	while (++i < g.V)
+		ft_printf("Vertex [%d]: %s\n", i, v[i].name);
 }
