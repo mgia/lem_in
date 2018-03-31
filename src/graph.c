d8bbb734e51d6b541bc262154f2afaed9d4191b4
mtan <findmarcg@gmail.com>
2018-03-31 14:22:33 -0700

    Edited graph.c to take graph ptr

--- src/graph.c
+++ src/graph.c
@@ -13,15 +13,15 @@
 #include "lem_in.h"
 #include "list.h"
 #include "memory.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-void	add_edge(t_graph g, int p, int c)
+void	add_edge(t_graph *g, int p, int c)
 {
 	t_vertex	*l;
 	t_list		*child;
 
-
-	l = g.nodes;
+	l = g->nodes;
 	child = NULL;
 	child = ft_lstnew(NULL, 0);
 	child->content = &l[c];
@@ -31,27 +31,4 @@ void	add_edge(t_graph g, int p, int c)
 	else
 		ft_lstadd(&l[p].children, child);
 	c++;
-}
-
-// void	free_vertex(t_vertex v)
-// {
-// 	if (v.children)
-// 		free(v.children);
-// 	if (v.name)
-// 		free(v.name);
-// }
-
-void	free_graph(t_graph g)
-{
-	int		i;
-
-	i = -1;
-	while (++i < g.V)
-	{
-		if (g.nodes[i].children)
-			ft_lstdel(&g.nodes[i].children, ft_save_content);
-		// if (g.nodes[i].name)
-			// free(g.nodes[i].name);
-	}
-	ft_memdel((void **)&g.nodes);
 }
