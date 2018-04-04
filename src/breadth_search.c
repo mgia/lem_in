54d08ad53ada6b9347305caf6346f2427e14fe60
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-03 21:11:05 -0700

    Refactor

--- src/breadth_search.c
+++ src/breadth_search.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/02 19:39:46 by ivankozlov        #+#    #+#             */
-/*   Updated: 2018/04/03 13:30:50 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/03 21:08:17 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,7 @@
 #include "numbers.h"
 #include "ft_printf.h"
 #include "list.h"
-
-// // utility function for finding paths in graph
-// // from source to destination
-// void findpaths(vector<vector<int> >&g, int src, 
-//                                  int dst, int v)
-// {
-//     // create a queue which stores
-//     // the paths
-//     queue<vector<int> > q;
  
-//     // path vector to store the current path
-//     vector<int> path;
-//     path.push_back(src);
-//     q.push(path);
-//     while (!q.empty()) {
-//         path = q.front();
-//         q.pop();
-//         int last = path[path.size() - 1];
- 
-//         // if last vertex is the desired destination
-//         // then print the path
-//         if (last == dst) 
-//             printpath(path);        
- 
-//         // traverse to all the nodes connected to 
-//         // current vertex and push new path to queue
-//         for (int i = 0; i < g[last].size(); i++) {
-//             if (isNotVisited(g[last][i], path)) {
-//                 vector<int> newpath(path);
-//                 newpath.push_back(g[last][i]);
-//                 q.push(newpath);
-//             }
-//         }
-//     }
-// }
-
 void	init_path(t_path *p, size_t size)
 {
 	p->size = 0;
@@ -64,11 +29,52 @@ void	path_push_back(t_path *p, int el)
 	p->size++;
 }
 
+t_path	*path_create_copy(t_path p, size_t size)
+{
+	int		i;
+	t_path	*res;
+
+	res = ft_memalloc(sizeof(t_path));
+	init_path(res, size);
+	i = -1;
+	while (++i < (int)p.size)
+		res->array[i] = p.array[i];
+	res->size = p.size;
+	return (res);
+}
+
+void	del_queue_item(void *item, size_t size)
+{
+	t_path	tmp;
+
+	tmp = *(t_path *)item;
+	ft_memdel((void **)&tmp.array);
+	ft_free_content(item, size);
+}
+
+void	iterate_children(t_vertex node, t_path path, size_t path_max_size)
+{
+	int		kid;
+	t_path	*tmp;
+
+	while (node.children)
+	{
+		kid = (*(t_vertex *)node.children->content).number;
+		if (is_in_arr(path.array, path.size, kid) < 0)
+		{
+			tmp = path_create_copy(path, path_max_size);
+			path_push_back(tmp, kid);
+			q_push(tmp, sizeof(t_path));
+			ft_memdel((void **)&tmp);
+		}
+		node.children = node.children->next;
+	}
+}
+
 t_list	*breadth_search(t_graph g, t_vertex c, t_vertex e)
 {
 	t_path		path;
-	t_list		*tmp;
-	t_vertex	last;
+	t_path		*p;
 	t_list		*paths;
 
 	paths = NULL;
@@ -78,36 +84,17 @@ t_list	*breadth_search(t_graph g, t_vertex c, t_vertex e)
 	q_push(&path, sizeof(t_path));
 	while (q_peek())
 	{
-		path = *(t_path *)q_pop();
-		ft_printf("Popped path for queue\n");
-		print_int_arr(path.array, path.size);
-		q_print();
-		last = g.nodes[path.array[path.size - 1]];
-		if (last.number == e.number)
-		{
-			// ft_printf("Found path\n");
-			// print_int_arr(path.array, path.size);
-			tmp = ft_lstnew(path.array, path.size * sizeof(int));
-			paths == NULL ? paths = tmp : ft_lstaddback(&paths, tmp);
-		}
-		while (last.children)
-		{
-			if (is_in_arr(path.array, path.size, (*(t_vertex *)last.children->content).number) < 0)
-			{
-				path_push_back(&path,
-					(*(t_vertex *)last.children->content).number);
-				ft_printf("Adding path to queue\n");
-				print_int_arr(path.array, path.size);
-				q_push(&path, sizeof(t_path));
-				q_print();
-				path.size--;
-			}
-			last.children = last.children->next;
-		}
+		p = (t_path *)q_pop();
+		c = g.nodes[p->array[p->size - 1]];
+		if (c.number == e.number && paths == NULL)
+			paths = ft_lstnew(p->array, p->size * sizeof(int));
+		else if (c.number == e.number && paths != NULL)
+			ft_lstaddback(&paths,
+				ft_lstnew(p->array, p->size * sizeof(int)));
+		iterate_children(c, *p, g.V);
+		ft_memdel((void **)&p->array);
+		ft_memdel((void **)&p);
 	}
-	q_destroy();
-	ft_memdel((void **)&path.array);
-	if (!paths)
-		ft_printf("No paths found\n");
+	q_destroy(del_queue_item);
 	return (paths);
 }
